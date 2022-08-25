81d056b3ba7d8b4f60079b8c5feea04a1e85701d
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-08-25 23:54:10 +0200

    guvenlik

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Home from './components/Home';
 import Kullan from './components/Kullan';
 import Login from './components/Login';
+//burada emniyet icin yapiyoruz
+import ProtectedRoute from './components/ProjectG';
 import Register from './components/Register'
 import {AuthContextProvider} from './context/AuthContext'
 
@@ -17,7 +19,11 @@ function App() {
     <Route path='/' element={<Home/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/register' element={<Register/>}/>
-    <Route path='/kullan' element={<Kullan/>}/>
+    <Route path='/kullan' element={
+    <ProtectedRoute>
+      <Kullan/>
+    </ProtectedRoute>
+    }/>
    </Routes>
 
     </div>
